0fb43f8ec91a5cb30ff65737a88aabf818d40960
Raul C <raul.gines1999@gmail.com>
2022-07-08 20:45:04 -0500

    arreglando boton sing out

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,13 @@ from "@reduxjs/toolkit";
 import products from './slices/products.slice'
 import isLoading from './slices/isLoading.slice'
 import categories from './slices/categories.slice'
+import cart from './slices/cart.slice'
 
 export default configureStore({
     reducer: {
         products,
         isLoading,
-        categories
+        categories,
+        cart
     }
 })
